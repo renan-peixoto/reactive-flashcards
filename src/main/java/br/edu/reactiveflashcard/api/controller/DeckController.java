346f3b757d3e2381312d06851c8e93b773d08ad3
Renan Peixoto <renan2.peixoto@gmail.com>
2023-10-17 18:14:25 -0300

    deck find by id

--- src/main/java/br/edu/reactiveflashcard/api/controller/DeckController.java
+++ src/main/java/br/edu/reactiveflashcard/api/controller/DeckController.java
@@ -2,8 +2,11 @@ package br.edu.reactiveflashcard.api.controller;
 
 import br.edu.reactiveflashcard.api.controller.request.DeckRequest;
 import br.edu.reactiveflashcard.api.controller.request.DeckResponse;
+import br.edu.reactiveflashcard.api.controller.response.UserResponse;
 import br.edu.reactiveflashcard.api.mapper.DeckMapper;
+import br.edu.reactiveflashcard.core.validation.MongoId;
 import br.edu.reactiveflashcard.domain.service.DeckService;
+import br.edu.reactiveflashcard.domain.service.query.DeckQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -23,6 +26,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class DeckController {
 
     public final DeckService deckService;
+    private final DeckQueryService deckQueryService;
     public final DeckMapper deckMapper;
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
@@ -31,5 +35,12 @@ public class DeckController {
         return deckService.save( deckMapper.toDocument( request ) )
                 .doFirst( () -> log.info( "==== saving deck with follow data {}", request ) )
                 .map(deckMapper::toResponse);
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE, value = "{id}")
+    public Mono<DeckResponse> findById(@PathVariable @Valid @MongoId(message = "{deckController.id}") final String id) {
+        return deckQueryService.findById( id )
+                .doFirst( () -> log.info( "==== finding a deck with follow id {}", id ) )
+                .map( deckMapper::toResponse );
     }
 }
---
+++ src/main/java/br/edu/reactiveflashcard/domain/service/query/DeckQueryService.java
@@ -0,0 +1,28 @@
+package br.edu.reactiveflashcard.domain.service.query;
+
+import br.edu.reactiveflashcard.domain.document.DeckDocument;
+import br.edu.reactiveflashcard.domain.exception.NotFoundException;
+import br.edu.reactiveflashcard.domain.repository.DeckRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+import static br.edu.reactiveflashcard.domain.exception.BaseErrorMessage.USER_NOT_FOUND;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class DeckQueryService {
+
+    private final DeckRepository deckRepository;
+
+    public Mono<DeckDocument> findById(final String id) {
+        return deckRepository.findById( id )
+                .doFirst( () -> log.info("==== try to find deck with id {}", id ))
+                .filter( Objects::nonNull )
+                .switchIfEmpty( Mono.defer(() -> Mono.error( new NotFoundException( USER_NOT_FOUND.params( id ).getMessage() ) ) ) );
+    }
+}
